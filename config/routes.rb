713cdb534d91cb8086b7e102b96616d54c3d7b88
SmulskyiIhor <smulia755@gmail.com>
2019-08-09 13:33:04 +0300

    add admin rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'sessions/create'
   post 'authenticate', to: 'sessions#create'
 end
